46160c96321e02a82b0ccbbee5b2467fda5ea7d6
aovoq <a21dc163@dhw.ac.jp>
2023-07-29 18:43:10 +0900

    retina対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Box, Button, Image, css } from '@kuma-ui/core'
+import { Box, Button, Image, Text, css } from '@kuma-ui/core'
 import { toPng } from 'html-to-image'
 import { useCallback, useRef } from 'react'
 import Drop from './components/Drop'
 import { useAtom } from 'jotai'
 import Settings from './components/Settings'
 import { fileAtom, settingAtom } from './jotai'
-
 
 function App() {
   const canvasRef = useRef<HTMLDivElement>(null)
@@ -32,7 +31,7 @@ function App() {
   const bundleExport = useCallback(() => {
     console.log('export', canvasRef.current)
     if (canvasRef.current === null) return
-    toPng(canvasRef.current, { cacheBust: true })
+    toPng(canvasRef.current, { cacheBust: true, pixelRatio: 1 })
       .then((dataUrl) => {
         const link = document.createElement('a')
         link.download = 'line_rich-menu.png'
@@ -49,18 +48,46 @@ function App() {
       <Drop />
       <Settings />
       {file && (
-          <Button
-          p={8}
-          bg='black'
-          color='white'
-          borderRadius={0}
-          _hover={{ opacity: 0.8 }}
-          onClick={bundleExport}
+        <>
+          <Text>Preview ↓</Text>
+          <Box
+            transform='scale(0.4)'
+            // transformOrigin='top left'
+            m={`calc(-${setting.height}px / 10 * 3) calc(-${setting.width}px / 10 * 3)`}
+            bg={setting.paddingColor}
+            pt={`${setting.paddingTop}px`}
+            pl={`${setting.paddingLeft}px`}
+            pb={`${setting.paddingBottom}px`}
+            pr={`${setting.paddingRight}px`}
+            width={`${setting.width}px`}
+            height={`${setting.height}px`}
+            className={css`
+              box-sizing: border-box;
+            `}
           >
+            <Image
+              src={file.toString()}
+              width='100%'
+              height='100%'
+              borderRadius={`${setting.corner}px`}
+              className={css`
+                object-fit: cover;
+                object-position: center;
+                pointer-events: none;
+              `}
+              display='block'
+            />
+          </Box>
+          <Button py={8} px={16}  bg='black' color='white' borderRadius={0} _hover={{ opacity: 0.8 }} onClick={bundleExport}>
             export
           </Button>
-        )}
-      <Box as='main' display='flex' flexDir='column' alignItems='flex-start'>
+        </>
+      )}
+      <Box
+        position='absolute'
+        top='-9999px'
+        left='-9999px'
+      >
         <div ref={canvasRef}>
           <Box
             bg={setting.paddingColor}
